336fa3fad3aa79ba5509b9e7f1cdcdc5c11eea46
Alexander Kiselev <alexander.kiselev@mail.ru>
2018-03-14 10:34:42 +0300

    Added save to bitrix

--- docs/local/templates/2018/sendform.php
+++ docs/local/templates/2018/sendform.php
@@ -1,12 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alexander
- * Date: 05.02.18
- * Time: 0:14
- */
+define('STOP_STATISTICS', true);
+require_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');
+$GLOBALS['APPLICATION']->RestartBuffer();
 
 header('content/type: application/json');
+
 
 $to = [
     'cni-day@ya.ru',
@@ -15,18 +13,56 @@ $to = [
     'alexander.kiselev@mail.ru'
 ];
 
+$data = $_POST;
+
 $to = ($data['sendto']) ? explode(',', $data['sendto']) : $to;
+
 $product = (isset($data['product']) && $data['product'] !== 0)
     ? 'Продукт: день ' . $data['product']
     : 'Продукт: не выбран';
 
-$data = $_POST;
 $city = $data['city'];
 $name = $data['form']['data'][0][1];
 $phone = $data['form']['data'][1][1];
 $text = $data['form']['data'][2][1];
 $date = date('d.m.Y H:i:s');
 
+
+CModule::IncludeModule("iblock");
+
+/**
+ * Created by PhpStorm.
+ * User: alexander
+ * Date: 05.02.18
+ * Time: 0:14
+ */
+
+
+
+
+/**
+ * Save to bitrix
+ */
+$el = new CIBlockElement;
+
+$PROP = array();
+$PROP['NAME'] = $name;
+$PROP['PHONE'] = $phone;
+$PROP['TEXT'] = $text;
+$PROP['CITY'] = $city;
+
+$arLoadProductArray = Array(
+    "IBLOCK_SECTION_ID" => false,          // элемент лежит в корне раздела
+    "IBLOCK_ID"      => 3,
+    "PROPERTY_VALUES"=> $PROP,
+    "NAME"           => $name,
+    "ACTIVE"         => "Y",            // активен
+);
+
+
+/**
+ * Send email
+ */
 $message = "
     <h1>Заявка на сайте $city</h1>
     <hr />
